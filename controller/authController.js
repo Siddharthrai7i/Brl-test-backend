9f53c6453f97cd1c136561d71b218bb1a75ec045
Palak2820 <54177848+Palak2820@users.noreply.github.com>
2020-04-23 12:57:42 +0530

    validation

--- controller/authController.js
+++ controller/authController.js
@@ -1,6 +1,20 @@
 const User = require('../model/User');
+const Joi =require('@hapi/joi');
+
+const schema =Joi.object({
+    name:Joi.string().required(),
+    
+    email: Joi.string().email().required(),
+
+    password:Joi.string().min(5).required()
+});
+
 
 module.exports.register = async (req, res) => {
+
+   const {error} = schema.validate(req.body);
+   if(error) return res.status(400).send(error.details[0].message);
+
     const { name, email, password } = req.body;
     new User({
         name: name,
