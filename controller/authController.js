ff6b1838666963fc363be77c9e63888f118dc505
Palak2820 <54177848+Palak2820@users.noreply.github.com>
2020-04-23 13:09:40 +0530

    validate

--- controller/authController.js
+++ controller/authController.js
@@ -15,6 +15,8 @@ module.exports.register = async (req, res) => {
    const {error} = schema.validate(req.body);
    if(error) return res.status(400).send(error.details[0].message);
 
+   const emailExist =await User.findOne({email:req.body.email});
+   if(emailExist) return res.status(400).send('email already exists');
     const { name, email, password } = req.body;
     new User({
         name: name,
