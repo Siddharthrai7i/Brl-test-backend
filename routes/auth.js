03e1b351ed217fc37f4aeb20a7ed70556d35df16
Palak2820 <54177848+Palak2820@users.noreply.github.com>
2020-04-23 10:38:25 +0530

    initial commit

---
+++ routes/auth.js
@@ -0,0 +1,37 @@
+const router=require('express').Router();
+const User =require('../model/User');
+
+//validation
+//const Joi =require('@hapi/joi');
+
+
+  // const schema =Joi.object({
+    //name:Joi.string().required(),
+    //email:Joi.string().min(6).required().email(),
+    //password:Joi.string().min(5).required()
+  //}); 
+
+
+router.post('/register',async (req,res)=>{
+  const user=new User({
+    name:req.body.name,
+    email:req.body.email,
+    password:req.body.password
+  });
+  try{
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err){
+    res.status(400).send(err);
+  }
+
+});
+
+
+
+
+
+
+
+
+module.exports =router;
