42044dc0299ba58312e1a3ae394ab7abd3d97208
Palak2820 <54177848+Palak2820@users.noreply.github.com>
2020-06-16 00:24:14 +0530

    CORS

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 // CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  red.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   next()
 })
