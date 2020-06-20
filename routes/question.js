2a9845c1906612350e90ad1b07355d091f8e9eed
Vishesh <vishesh.dh1@gmail.com>
2020-06-21 01:00:39 +0530

    displaying 10 random questions

--- routes/question.js
+++ routes/question.js
@@ -15,22 +15,42 @@ router.post('/check-answers', questionController.postCheckAnswers);
 router.get('/get-questions', auth, async (req, res, next) => {
     
     const user = await User.findById(req.user.id)
-
-    // await Question.countDocuments({},(err, cnt) => {
-    //     console.log(cnt);
-    // })
-
     const all_questions = await Question.find();
 
+    // get total number of docs
+    const total_docs = all_questions.length
+
+    nos_list = []
+    for (let i = 0; i <total_docs; i++) 
+        nos_list.push(i)
+
+
+    random_list = []
+    for (let i = 0; i < 10; i ++) {
+        let s1 = nos_list.length
+        let s2 = Math.random() * s1
+        let res = Math.floor(s2)
+        
+        // rind = res
+
+        let randomNumber = nos_list[res]
+        nos_list.splice(res, 1)
+        random_list.push(randomNumber)
+    }
+    /////////////////////////
+
+    res_questions = []
     temp_questions_arr = []
-    for (let i = 0; i < all_questions.length; i++) {
-        temp_questions_arr.push(all_questions[i]._id)
+
+    for (let i = 0; i < random_list.length; i++) {
+        temp_questions_arr.push(all_questions[random_list[i]]._id)
+        res_questions.push(all_questions[random_list[i]])
     }
     
     user.questions = temp_questions_arr
     user.save()
     
-    return res.status(200).json({all_questions})
+    return res.status(200).json({res_questions})
     
 });
 
