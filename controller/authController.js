91c02bf2824eb9cfa49377080317bdbe72fb34e0
amolsr <amol.saini567@gmail.com>
2020-08-16 18:03:23 +0530

    auth fucntion corrected

--- controller/authController.js
+++ controller/authController.js
@@ -1,81 +1,88 @@
 const jwt = require("jsonwebtoken");
 const User = require("../model/User");
 
-const checkToken = (req, res, next) => {
-  let bearertoken = req.headers['authorization']
-  const token = bearertoken.replace('Bearer ', '')
-  if (typeof token !== "undefined") {
-    try {
-      const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-      req.user = decoded.user;
-      next();
-    } catch (err) {
-      console.log(err);
-      res.status(401).json({ msg: "Token is not valid" });
-    }
+const checkToken = (req) => {
+  const header = req.headers["authorization"];
+  if (typeof header !== "undefined") {
+    const bearer = header.split(" ");
+    return { success: true, token: bearer[1] };
   } else {
-    res.status(401).json({ msg: "No token, authorization denied" });
+    return { success: false };
   }
 };
 
 exports.loginStudent = (req, res) => {
   const { rollNumber, password } = req.body;
   User.findOne({ rollNumber }).then((user) => {
-      if (user) {
-        if (!user.isLoggedIn){
-          if (user.password === password) {
-            jwt.sign(
-              { user: { id: user.id } },
-              process.env.TOKEN_SECRET,
-              { expiresIn: "1h" },
-              async (err, token) => {
-                user.isLoggedIn = true;
-                await user.save();
-                res.json({ 
-                  token: token,
-                  timer: {
-                    hours: 00,
-                    minutes: 50,
-                    seconds: 00
-                  }
-                });
-              }
-            );
-          } else {
-            res.status(401).json({ error: "Invalid Password" });
-          }
+    if (user) {
+      if (!user.isLoggedIn) {
+        if (user.password === password) {
+          jwt.sign(
+            { user: { id: user.id } },
+            process.env.TOKEN_SECRET,
+            { expiresIn: "1h" },
+            async (err, token) => {
+              user.isLoggedIn = true;
+              await user.save();
+              res.json({
+                token: token,
+                timer: {
+                  hours: 00,
+                  minutes: 50,
+                  seconds: 00,
+                },
+              });
+            }
+          );
         } else {
-          res.status(400).json({
-            error: 'User already Logged In'
-          });
+          res.status(401).json({ error: "Password Incorrect" });
         }
+      } else {
+        res.status(400).json({
+          error: "User Already Logged In",
+        });
+      }
     } else {
-      res.status(400).json({ error: "Invalid User" });
+      res.status(400).json({ error: "No User Exist" });
     }
-  })
+  });
 };
 
 exports.authStudent = (req, res, next) => {
-  checkToken(req, res, next);
+  var result = await checkToken(req);
+  if (result.success === true && result.token != undefined) {
+    try {
+      const decoded = await jwt.verify(result.token, process.env.jwtSecret);
+      if (decoded.user) {
+        req.user = decoded.user;
+        next();
+      } else {
+        return res.status(401).json({ success: false , msg: "Token Is Not Valid" });
+      }
+    } catch (ex) {
+      return res.status(403).json({ success: false , msg: "Token Is Not Valid" });
+    }
+  } else {
+    return res.status(403).json({ success: false, msg: "Token Is Not Valid" });
+  }
 };
 
-exports.checkStartTime = (req,res,next) => {
- // if(Date.now() >= Date.UTC(2020,07,18,08,30)) {
-    next();
+exports.checkStartTime = (req, res, next) => {
+  // if(Date.now() >= Date.UTC(2020,07,18,08,30)) {
+  next();
   //} else {
   //  res.status(400).json({
-  //    message: "Test Has not Been Started"
-   // });
- // }
+  //    message: "Test Not Yet Started"
+  // });
+  // }
+};
 
-}
-
-exports.checkEndTime = (req,res,next) => {
-//  if (Date.now() <= Date.UTC(2020,07,18,09,10)){
-    next();
-//  } else {
-//    res.status(400).json({
-//      message: "Test has Ended"
-//    });
-//  }
-}
+exports.checkEndTime = (req, res, next) => {
+  //  if (Date.now() <= Date.UTC(2020,07,18,09,10)){
+  next();
+  //  } else {
+  //    res.status(400).json({
+  //      message: "Test has Ended"
+  //    });
+  //  }
+};
