d05dbc4884b721d70dad8d1a18e907e3543f439a
gracygupta <hzgracy@gmail.com>
2023-09-02 15:45:16 +0530

    update aggregate pipeline for result generation

--- controller/userController.js
+++ controller/userController.js
@@ -12,7 +12,7 @@ exports.addUser = async (req, res) => {
   if (rollNumberExists)
     return res.status(400).json({ error: "roll number already exists" });
 
-  user = new User({
+  let user = new User({
     name,
     rollNumber,
     phoneNumber,
@@ -39,4 +39,187 @@ exports.unfair = async (req, res, next) => {
     console.log(err);
     res.status(400);
   }
+};
+
+exports.aggregateUsers = () => {
+  return new Promise((resolve, reject) => {
+    User.aggregate([
+      {
+        $match: { responses: { $exists: true } },
+      },
+      {
+        $addFields: {
+          totalQuestions: { $size: "$questions" }, // Count the total number of questions for the user
+        },
+      },
+      {
+        $unwind: {
+          path: "$responses",
+          preserveNullAndEmptyArrays: false,
+        },
+      },
+      {
+        $lookup: {
+          from: "questions",
+          let: { questionid: { $toObjectId: "$responses.question" } },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $eq: ["$$questionid", "$_id"],
+                },
+              },
+            },
+          ],
+          as: "correct",
+        },
+      },
+      { $unwind: "$correct" },
+      {
+        $match: {
+          choices: {
+            $elemMatch: { $ne: "" },
+          },
+        },
+      },
+      {
+        $addFields: {
+          scoreChange: {
+            $cond: [
+              {
+                $in: [
+                  "$correct.category",
+                  {
+                    $ifNull: ["$choices", []],
+                  },
+                ],
+              },
+              {
+                $cond: [
+                  { $eq: ["$correct.correct", "$responses.response"] },
+                  4, // +4 for correct answer in the dynamic category
+                  -1.0, // -1.00 for incorrect answer in the dynamic category
+                ],
+              },
+              {
+                $cond: [
+                  { $eq: ["$correct.correct", "$responses.response"] },
+                  4, // +4 for correct answer
+                  -0.25, // -0.25 for incorrect answer in other categories
+                ],
+              },
+            ],
+          },
+        },
+      },
+      {
+        $group: {
+          _id: "$_id",
+          name: { $first: "$name" },
+          phoneNumber: { $first: "$phoneNumber" },
+          email: { $first: "$email" },
+          rollNumber: { $first: "$rollNumber" },
+          branch: { $first: "$branch" },
+          skills: { $first: "$skills" },
+          switchCounter: { $first: "$switchCounter" },
+          totalQuestions: { $first: "$totalQuestions" },
+          score: { $sum: "$scoreChange" },
+          aptitude: {
+            $sum: {
+              $cond: [
+                { $eq: ["$correct.category", "aptitude".toUpperCase()] },
+                "$scoreChange",
+                0,
+              ],
+            },
+          },
+          html_css: {
+            $sum: {
+              $cond: [
+                { $eq: ["$correct.category", "html/css".toUpperCase()] },
+                "$scoreChange",
+                0,
+              ],
+            },
+          },
+          programming: {
+            $sum: {
+              $cond: [
+                { $eq: ["$correct.category", "programming".toUpperCase()] },
+                "$scoreChange",
+                0,
+              ],
+            },
+          },
+          networking: {
+            $sum: {
+              $cond: [
+                { $eq: ["$correct.category", "networking".toUpperCase()] },
+                "$scoreChange",
+                0,
+              ],
+            },
+          },
+          aiml: {
+            $sum: {
+              $cond: [
+                { $eq: ["$correct.category", "aiml".toUpperCase()] },
+                "$scoreChange",
+                0,
+              ],
+            },
+          },
+          blockchain: {
+            $sum: {
+              $cond: [
+                { $eq: ["$correct.category", "blockchain".toUpperCase()] },
+                "$scoreChange",
+                0,
+              ],
+            },
+          },
+          bonus: {
+            $sum: {
+              $cond: [
+                {
+                  $in: [
+                    { $toUpper: "$correct.category" },
+                    {
+                      $map: {
+                        input: "$choices",
+                        as: "choice",
+                        in: { $toUpper: "$$choice" },
+                      },
+                    },
+                  ],
+                },
+                "$scoreChange",
+                0,
+              ],
+            },
+          },
+        },
+      },
+      {
+        $addFields: {
+          // Multiply the score by 4 and then normalize it to 100 based on the total number of questions
+          normalizedScore: {
+            $multiply: [
+              100,
+              { $divide: ["$score", { $multiply: ["$totalQuestions", 4] }] },
+            ],
+          },
+        },
+      },
+      {
+        $out: "results",
+      },
+    ])
+      .then((result) => {
+        resolve(result);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 };
--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const { body } = require("express-validator");
 const User = require("../model/User");
 const utilController = require("../controller/utilController");
+const mongoose = require('mongoose');
 
 router.post("/check-answers", questionController.postCheckAnswers);
 
@@ -17,85 +18,20 @@ router.post("/add-question", questionController.addQuestions);
 // router.post("/users", userController.getUsers);
 
 
+
+
 router.get("/result", (req, res) => {
-  User.aggregate([
-    {
-      $match: { responses: { $exists: true } }
-    },
-    {
-      $unwind: {
-        path: "$responses",
-        preserveNullAndEmptyArrays: false,
-      }
-    },
-    {
-      $lookup: {
-        from: "questions",
-        let: { questionid: { $toObjectId: "$responses.question" } },
-        pipeline: [
-          {
-            $match: {
-              $expr: {
-                $eq: ["$$questionid", "$_id"],
-              }
-            }
-          }
-        ],
-        as: "correct"
-      }
-    },
-    { $unwind: "$correct" },
-    {
-      $addFields: {
-        scoreChange: {
-          $cond: [
-            { $eq: ["$correct.category", "bonus".toUpperCase()] },
-            {
-              $cond: [
-                { $eq: ["$correct.correct", "$responses.response"] },
-                4, // +4 for correct answer in bonus category
-                -1.00 // -1.25 for incorrect answer in bonus category
-              ]
-            },
-            {
-              $cond: [
-                { $eq: ["$correct.correct", "$responses.response"] },
-                4, // +4 for correct answer
-                -0.25 // -0.25 for incorrect answer in other categories
-              ]
-            }
-          ]
-        }
-      }
-    },
-    {
-      $group: {
-        _id: "$_id",
-        name: { $first: "$name" },
-        phoneNumber: { $first: "$phoneNumber" },
-        email: { $first: "$email" },
-        rollNumber: { $first: "$rollNumber" },
-        branch: { $first: "$branch" },
-        skills: { $first: "$skills" },
-        switchCounter: { $first: "$switchCounter" },
-        score: { $sum: "$scoreChange" },
-        aptitude: { $sum: { $cond: [ { $eq: ["$correct.category", "aptitude".toUpperCase()] }, "$scoreChange", 0 ] } },
-        html_css: { $sum: { $cond: [ { $eq: ["$correct.category", "html/css".toUpperCase()] }, "$scoreChange", 0 ] } },
-        programming: { $sum: { $cond: [ { $eq: ["$correct.category", "programming".toUpperCase()] }, "$scoreChange", 0 ] } },
-        networking: { $sum: { $cond: [ { $eq: ["$correct.category", "networking".toUpperCase()] }, "$scoreChange", 0 ] } },
-        aiml: { $sum: { $cond: [ { $eq: ["$correct.category", "aiml".toUpperCase()] }, "$scoreChange", 0 ] } },
-        blockchain: { $sum: { $cond: [ { $eq: ["$correct.category", "blockchain".toUpperCase()] }, "$scoreChange", 0 ] } },
-        bonus: { $sum: { $cond: [ { $eq: ["$correct.category", "bonus".toUpperCase()] }, "$scoreChange", 0 ] } }
-      }
-    }
-  ]
-  )
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  userController.aggregateUsers()
+  .then(() => {
+    return mongoose.connection.db.collection('results').find({}).toArray();
+  })
+  .then((result) => {
+    return res.status(200).json({result})
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send("Error occurred during aggregation.");
+  });
 });
 
 const Test = require("../model/Time");
