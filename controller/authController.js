a77298b30e93b1d05c4d8e6f0095e320dadec760
gracygupta <hzgracy@gmail.com>
2023-09-10 02:31:37 +0530

    impliment recaptcha on non protected routes

--- controller/authController.js
+++ controller/authController.js
@@ -1,6 +1,8 @@
 const jwt = require("jsonwebtoken");
 const User = require("../model/User");
 const Test = require("../model/Time");
+const axios = require("axios");
+require("dotenv").config();
 
 const checkToken = (req) => {
   const header = req.headers["authorization"];
@@ -13,36 +15,50 @@ const checkToken = (req) => {
 };
 
 exports.loginStudent = (req, res) => {
-  const { rollNumber, password } = req.body;
-  User.findOne({ rollNumber }).then((user) => {
-    if (user) {
-      if (!user.isLoggedIn) {
-        if (user.password === password) {
-          jwt.sign(
-            { user: { id: user.id } },
-            process.env.TOKEN_SECRET,
-            { expiresIn: "1h" },
-            async (err, token) => {
-              user.isLoggedIn = true;
-              await user.save();
-              res.json({
-                token: token,
-                time: {
-                  hours: 00,
-                  minutes: 60,
-                  seconds: 00,
-                },
+  const { rollNumber, password, token } = req.body;
+  axios({
+    url: `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.CAPTCHA_SECRET}&response=${token}`,
+    method: "POST",
+  }).then((data) => {
+    if (data.success) {
+      User.findOne({ rollNumber })
+        .then((user) => {
+          if (user) {
+            if (!user.isLoggedIn) {
+              if (user.password === password) {
+                jwt.sign(
+                  { user: { id: user.id } },
+                  process.env.TOKEN_SECRET,
+                  { expiresIn: "1h" },
+                  async (err, token) => {
+                    user.isLoggedIn = true;
+                    await user.save();
+                    res.json({
+                      token: token,
+                      time: {
+                        hours: 00,
+                        minutes: 60,
+                        seconds: 00,
+                      },
+                    });
+                  }
+                );
+              } else {
+                res.status(401).json({ error: "Password Incorrect" });
+              }
+            } else {
+              res.status(400).json({
+                error: "User Already Logged In",
               });
             }
-          );
-        } else {
-          res.status(401).json({ error: "Password Incorrect" });
-        }
-      } else {
-        res.status(400).json({
-          error: "User Already Logged In",
+          } else {
+            res.status(400).json({ error: "You are not a Human" });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(400).json({ error: "Something went wrong" });
         });
-      }
     } else {
       res.status(400).json({ error: "No User Exist" });
     }
@@ -72,48 +88,47 @@ exports.authStudent = async (req, res, next) => {
   }
 };
 
-exports.checkTime = (req,res,next)=>{
-  try{
-	  Test.findOne({ title: "BRL Recruitment Test"}, function (err, result) {
+exports.checkTime = (req, res, next) => {
+  try {
+    Test.findOne({ title: "BRL Recruitment Test" }, function (err, result) {
       var time = new Date();
       if (err) {
         console.log(err);
         res.status(500).send("Internal Server Error");
-		  }
-		  else {
-			  if (time > result.startTime) {
-				  if (time < result.endTime) {
-					  const remainingTime = (result.endTime - time);
-            const minutes = Math.max(0,Math.floor(remainingTime / 60000));
-            const seconds = Math.max(0,Math.floor((remainingTime % 60000) / 1000));
+      } else {
+        if (time > result.startTime) {
+          if (time < result.endTime) {
+            const remainingTime = result.endTime - time;
+            const minutes = Math.max(0, Math.floor(remainingTime / 60000));
+            const seconds = Math.max(
+              0,
+              Math.floor((remainingTime % 60000) / 1000)
+            );
             req.time = {
               minutes: minutes,
-              seconds:seconds
-            }
-					  next();
-				  }
-				  else {
-					  res.status(500).json({
-						  message: "Test has Ended",
-					  });
-				  }
-		 	  }
-			  else {
-				  message = "Test Not Yet Started";
-				  var time_to_start = result.startTime - time;
+              seconds: seconds,
+            };
+            next();
+          } else {
+            res.status(500).json({
+              message: "Test has Ended",
+            });
+          }
+        } else {
+          message = "Test Not Yet Started";
+          var time_to_start = result.startTime - time;
           const minutes = Math.floor(time_to_start / 60000);
           const seconds = Math.floor((time_to_start % 60000) / 1000);
-				  res.status(500).json({
-					  message: message,
+          res.status(500).json({
+            message: message,
             minutes: minutes,
-            seconds:seconds
-				  });
-			  }
-		  }
-    })
-  }catch(err){
+            seconds: seconds,
+          });
+        }
+      }
+    });
+  } catch (err) {
     console.log(err);
-    res.status(500).json({message: "Internal Server Error"});
+    res.status(500).json({ message: "Internal Server Error" });
   }
-}
-
+};
